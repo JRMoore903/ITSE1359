82e697e06f4dd16dd03882d94b031a4f9bb8c156
JRMoore87 <moorjay1662@go.kilgore.edu>
2020-10-28 18:09:00 +0000

    Initial work

---
+++ main.py
@@ -0,0 +1,15 @@
+secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+number = int(input("What is the secret number?:"))
+while number != 777:
+    if number > secret_number:
